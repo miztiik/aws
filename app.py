11ec308a32d0892fa5858d90acdd5b68d5c6b7ce
Balkaran Brar <balkaran@trinimbus.com>
2020-02-10 23:02:38 -0500

    updated

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 from aws_cdk import core
 
 from msa.s3_stack import S3Stack
+from msa.another_S3 import testS3
 
 
 app = core.App()
 S3Stack(app, "s3")
+testS3(app,"tests3")
 
 app.synth()
--- msa/s3_stack.py
+++ msa/s3_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_s3 as s3,
+    aws_cloudfront as cdn,
     core
 ) 
 
@@ -9,10 +10,23 @@ class S3Stack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
-        prj_name = self.node.try_get_context("project_name")
-        imagesBucket = s3.Bucket(self, "images",
+        #prj_name = self.node.try_get_context("project_name")
+        
+        webHostingBucket = s3.Bucket(self, "webhosting-bucket",
             access_control=s3.BucketAccessControl.PRIVATE,
-            encryption=s3.BucketEncryption.KMS_MANAGED,
-            bucket_name=prj_name + '-images',
+            encryption=s3.BucketEncryption.KMS_MANAGED
+        )
+        #myBucket = core.Fn.import_value('tests3')
+        
+        webDistribution = cdn.CloudFrontWebDistribution(self,"webhosting-cdn",
+            origin_configs=[cdn.SourceConfiguration(
+                behaviors=[cdn.Behavior(is_default_behavior=True)],
+                s3_origin_source=cdn.S3OriginConfig(
+                    s3_bucket_source=webHostingBucket,
+                    origin_access_identity=cdn.OriginAccessIdentity(self,'webhosting-origin')
+                )
+            )]
 
         )
+        
+            
